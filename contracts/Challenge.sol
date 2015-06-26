bdeb7f099106f04d3a9aed234769ebca6bf0275c
Joel Torstensson <oed3933@gmail.com>
2015-06-26 09:59:22 -0400

    More feilds in challange.

--- contracts/Challenge.sol
+++ contracts/Challenge.sol
@@ -1,7 +1,10 @@
 contract Challenge {
 
-    bytes32 public challengeHash;
-    bytes32 public challengeHint;
+    bytes32 public hash;
+    bytes32 public title;
+    bytes32 public hint;
+    bytes32 public lat;
+    bytes32 public lng;
     address owner;
     address public winner;
 
@@ -9,16 +12,20 @@ contract Challenge {
         owner = msg.sender;
     }
 
-    function setChallenge(bytes32 hash, bytes32 hint) {
+    function setChallenge(bytes32 _hash, bytes32 _title, bytes32 _hint,
+                            bytes32 _lat, bytes32 _lng) {
         if (msg.sender == owner) {
-            challengeHash = hash;
-            challengeHint = hint;
+            hash = _hash;
+            title = _title;
+            hint = _hint;
+            lat = _lat;
+            lng = _lng;
         }
     }
 
     function check(bytes32 password) {
-        var hash = sha3(password);
-        if (challengeHash == hash) {
+        var h = sha3(password);
+        if (hash == h) {
             winner = msg.sender;
         }
     }
